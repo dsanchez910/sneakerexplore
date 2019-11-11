11440b85beab31cb907c3cbbbf04e59326a66b8a
Danny Sanchez <dsanchez910@gmail.com>
2019-11-11 15:02:15 -0500

    added sign in method to app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require("path");
 var app = express();
 app.use(express.static(path.join(__dirname,"/html")));
 
+app.post('/signin', function (req, res) {
+  
+})
+
 app.listen(3000,function(){
     console.log("Started listening on port", 3000);
 })

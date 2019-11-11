9c7cce78cfeb9078b037cbdc67308a98917ceda9
Danny Sanchez <dsanchez910@gmail.com>
2019-11-11 17:42:27 -0500

    added call back for user log in credentials

--- app.js
+++ app.js
@@ -2,36 +2,42 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var user = require('./user')
- 
+
 var app = express();
+
 app.use(express.static(path.join(__dirname,"/html")));
 
 app.use(bodyParser.json());
 
 app.post('/signin', function (req, res) {
-    var user_name=req.body.email;
-    var password=req.body.password;
-    if(user_name=='admin' && password=='admin'){
-        res.send('success');
+  var user_name=req.body.email;
+  var password=req.body.password;
+  user.validateSignIn(user_name,password,function(result){
+    if(result){
+      res.send('Success')
     }
     else{
-      res.send('Failure');
+      res.send('Wrong Credentials')
     }
-  })
+  });
+  
+  
+  
+})
 
-  app.post('/signup', function (req, res) {
-    var name=req.body.name;
-    var email=req.body.email;
-    var password=req.body.password;
-   
-    if(name && email && password){
-        user.signup(name, email, password)
-    }
-    else{
-      res.send('Failure');
-    }
-  })
+app.post('/signup', function (req, res) {
+  var name=req.body.name;
+  var email=req.body.email;
+  var password=req.body.password;
 
-app.listen(3000,function(){
+  if(name && email && password){
+  	user.signup(name, email, password)
+  }
+  else{
+  	res.send('Failure');
+  }
+})
+
+app.listen(7777,function(){
     console.log("Started listening on port", 3000);
 })

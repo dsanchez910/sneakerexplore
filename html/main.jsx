ada0c44069c128ebfed8daf709178e783eae9862
Danny Sanchez <dsanchez910@gmail.com>
2019-11-11 16:46:37 -0500

    added onchange event, bind in the class constructor, and defined it in the state variable

--- html/main.jsx
+++ html/main.jsx
@@ -6,12 +6,14 @@ var Link = window.ReactRouter.Link;
 class Signin extends React.Component {
     constructor(props) {
         super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
         this.state = {
-          email:'',
-          password:''
-        };
+            name:'',
+            email:'',
+            password:''
+          };
     }
     
     signIn(){
@@ -27,9 +29,12 @@ class Signin extends React.Component {
           });
         alert('Email address is ' + this.state.email + ' Password is ' + this.state.password);            
     }
+    handleNameChange(e){
+        this.setState({name:e.target.value})
+    }
     handleEmailChange(e){
         this.setState({email:e.target.value})
-    }
+    }   
     handlePasswordChange(e){
         this.setState({password:e.target.value})
     }
--- user.js
+++ user.js
@@ -1,1 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/sneakerexplore';
+
+module.exports = {
+    signup: function(){
+       
+    }
+}
